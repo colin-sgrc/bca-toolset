b4e81c92c0961c17469b9151dcf8ad8ee113a307
Colin Dyck <colin.robert.dyck@gmail.com>
2013-08-27 13:14:45 -0700

    update version 1.2.0.0 (manual process right now of updating assemblyinfo files...needs better process)

--- src/Installer/buildmsi.cmd
+++ src/Installer/buildmsi.cmd
@@ -1,5 +1,5 @@
 
-"C:\Program Files (x86)\WiX Toolset v3.6\bin\candle.exe" -dVersion=1.1.0.0 -dSource=D:\sgrc\projects\BCAAImport D:\sgrc\projects\rdkb\bcaa\Installer\BCADataAdviceToolset.wxs
+"C:\Program Files (x86)\WiX Toolset v3.6\bin\candle.exe" -dVersion=1.2.0.0 -dSource=C:\Users\cdyck\Documents\GitHub\BCAAImport C:\Users\cdyck\Documents\GitHub\bca-toolset\src\Installer\BCADataAdviceToolset.wxs
 
 
-"C:\Program Files (x86)\WiX Toolset v3.6\bin\light.exe" BCADataAdviceToolset.wixobj -ext WixNetFxExtension -cultures:en-us -out BCADataAdviceToolset.msi
+"C:\Program Files (x86)\WiX Toolset v3.6\bin\light.exe" BCADataAdviceToolset.wixobj -ext WixNetFxExtension -cultures:en-us -out BCADataAdviceToolset_1.2.0.0.msi
